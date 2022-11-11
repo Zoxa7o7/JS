7260ae6bde2623afcf3015591f7982890861a463
Zoxa7o7 <zahriddinbohramov@gmail.com>
2022-11-11 17:12:09 +0500

    first commit

---
+++ js.js
@@ -0,0 +1,37 @@
+var string = 'Hi Man Or Women'
+alert(string.toLocaleUpperCase())
+// var string = "SignUp"
+// alert (string)
+// var trueOrFalse = true
+// alert(trueOrFalse);
+
+var name1 = prompt("What is your name"),
+  surname = prompt("What is your surname"),
+  patronymic = prompt("What is your patronymic"),
+  age = prompt("How old are you"),
+  email = prompt("Email"),
+  password = prompt("Password"),
+  
+  
+  num1 = +prompt("5 + 3 = ")
+  console.log("5 + 3 = 8" + " - Ваш ответ: " + num1);
+  num2 = +prompt("5 - 3 = ")
+  console.log("5 - 3 = 2" + " - Ваш ответ: " + num2);
+  num3 = +prompt("5 * 3 = ")
+  console.log("5 * 3 = 15" + " - Ваш ответ: " + num3);
+  num4 = +prompt("15 / 3 = ")
+  console.log("15 / 3 = 5" + " - Ваш ответ: " + num4);
+  num5 = +prompt("10 % 3 = ")
+  console.log("10 % 3 = 1" + " - Ваш ответ: " + num4);
+  
+  alert("ОТКРОЙТЕ КОНСОЛЬ");
+  
+  
+  
+
+
+console.log(`My name is ${name1}, my surname is  ${surname}, my patronymic is  ${patronymic}, my age is  ${age} years old, email is${email},password is${password}`);
+
+
+// 1. Программа спрашивает у нас имя и возраст
+// 2.
